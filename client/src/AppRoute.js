5c9f984a3895154458c8f295a2331d09d949855f
Ibrahim Koujar <ibrahimkoujar91@gmail.com>
2019-05-22 23:53:54 +0300

    latest application.

--- client/src/AppRoute.js
+++ client/src/AppRoute.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
+/**
+ *  AppRoute Component.
+ * @param Component
+ * @param can
+ * @param redirect
+ * @param rest
+ */
 const AppRoute = ({component: Component, can = () => true, redirect, ...rest}) => (
     <Route {...rest} render = {(props) => (
         can() ? <Component {...props} /> : <Redirect to={redirect} />
--- client/src/views/Chat.js
+++ client/src/views/Chat.js
@@ -4,10 +4,14 @@ import { ContactHeader, Contacts, ChatHeader, Messages, MessageForm, UserProfile
 import socketIO from 'socket.io-client';
 import Auth from 'Auth';
 
+/**
+ * Chat Component.
+ */
 class Chat extends React.Component {
 
     state = { 
         contacts: [],
+        messages: [],
         contact: {},
         userProfile: false,
         profile: false,
@@ -20,153 +24,6 @@ class Chat extends React.Component {
         // Initialize socket.io connection.
         this.initSocketConnection();
     }
-
-    /**
-     * Initialize socket.io connection
-     */
-    initSocketConnection = () => {
-
-        // Connect to server and send user token.
-        let socket = socketIO(process.env.REACT_APP_SOCKET, {
-            query: 'token=' + Auth.getToken()
-        });
-
-        // Handle user connected event.
-        socket.on('connect', () => this.setState({connected: true}));
-
-        // Handle user disconnected event.
-        socket.on('disconnect', () => this.setState({connected: false}));
-
-        // Handle user data event (after connection).
-        socket.on('data', (user, contacts, messages, users) => {
-            let contact = contacts[0] || {};
-            this.setState({messages, contacts, user, contact}, () => {
-                this.updateUsersState(users);
-            });
-        });
-
-        // Handle new user event.
-        socket.on('new_user', this.onNewUser);
-        // Handle update user event.
-        socket.on('update_user', this.onUpdateUser);
-        // Handle incoming message event.
-        socket.on('message', this.onNewMessage);
-        // Handle changes for user presence.
-        socket.on('user_status', this.updateUsersState);
-        // Handle typing or composing event.
-        socket.on('typing', this.onTypingMessage);
-        // Handle socket.io errors.
-        socket.on('error', err => {
-            // If authentication error then logout.
-            if(err === 'auth_error'){
-                Auth.logout();
-                this.props.history.push('/login');
-            }
-        });
-        // Set user socket as state variable.
-        this.setState({socket});
-    };
-
-    /**
-     * Handle new user event.
-     * @param user
-     */
-    onNewUser = user => {
-        // Add user to contacts list.
-        let contacts = this.state.contacts.concat(user);
-        this.setState({contacts});
-    };
-
-    /**
-     * Handle update user event.
-     * @param user
-     */
-    onUpdateUser = user => {
-        // Add updated user is the current user then update local storage data.
-        if (this.state.user.id === user.id) {
-            this.setState({user});
-            Auth.setUser(user);
-            return;
-        }
-        // Update contact data.
-        let contacts = this.state.contacts;
-        contacts.forEach((element, index) => {
-            if(element.id === user.id) {
-                contacts[index] = user;
-                contacts[index].status = element.status;
-            }
-        });
-        this.setState({contacts});
-        if (this.state.contact.id === user.id) this.setState({contact: user});
-    };
-
-    /**
-     * Handle incoming message event.
-     * @param message
-     */
-    onNewMessage = message => {
-        // If user is already in chat then mark the message as seen.
-        if(message.sender === this.state.contact.id){
-            this.setState({typing: false});
-            this.state.socket.emit('seen', this.state.contact.id);
-            message.seen = true;
-        }
-        // Add message to messages list.
-        let messages = this.state.messages.concat(message);
-        this.setState({messages});
-    };
-
-    /**
-     * Handle typing or composing event.
-     * @param sender
-     */
-    onTypingMessage = sender => {
-        // If the typer not the current chat user then ignore it.
-        if(this.state.contact.id !== sender) return;
-        // Set typer.
-        this.setState({typing: sender});
-        // Create timeout function to remove typing status after 3 seconds.
-        clearTimeout(this.state.timeout);
-        const timeout = setTimeout(this.typingTimeout, 3000);
-        this.setState({timeout});
-    };
-
-    /**
-     * Clear typing status.
-     */
-    typingTimeout = () => this.setState({typing: false});
-
-    /**
-     * Send message.
-     * @param message
-     */
-    sendMessage = message => {
-        if(!this.state.contact.id) return;
-        message.receiver = this.state.contact.id;
-        let messages = this.state.messages.concat(message);
-        this.setState({messages});
-        this.state.socket.emit('message', message);
-    };
-
-    /**
-     * Send typing(composing) message.
-     */
-    sendType = () => this.state.socket.emit('typing', this.state.contact.id);
-
-    /**
-     * update users statuses.
-     * @param users
-     */
-    updateUsersState = users => {
-        let contacts = this.state.contacts;
-        contacts.forEach((element, index) => {
-            if(users[element.id]) contacts[index].status = users[element.id];
-        });
-        this.setState({contacts});
-        let contact = this.state.contact;
-        if(users[contact.id]) contact.status = users[contact.id];
-        this.setState({contact});
-    };
 
     /**
      * Handle navigation between chats.
@@ -240,7 +97,161 @@ class Chat extends React.Component {
         // Show only related messages.
         let messages = this.state.messages.filter(e => e.sender === contact.id || e.receiver === contact.id);
         return <Messages user={user} messages={messages} />
-    }
+    };
+
+    // Socket.IO Events --------------------------------------------------------//
+    /**
+     * Initialize socket.io connection
+     */
+    initSocketConnection = () => {
+        // Connect to server and send user token.
+        let socket = socketIO(process.env.REACT_APP_SOCKET, {
+            query: 'token=' + Auth.getToken()
+        });
+        // Handle user connected event.
+        socket.on('connect', () => this.setState({connected: true}));
+        // Handle user disconnected event.
+        socket.on('disconnect', () => this.setState({connected: false}));
+        // Handle user data event (after connection).
+        socket.on('data', this.onData);
+        // Handle new user event.
+        socket.on('new_user', this.onNewUser);
+        // Handle update user event.
+        socket.on('update_user', this.onUpdateUser);
+        // Handle incoming message event.
+        socket.on('message', this.onNewMessage);
+        // Handle changes for user presence.
+        socket.on('user_status', this.updateUsersState);
+        // Handle typing or composing event.
+        socket.on('typing', this.onTypingMessage);
+        // Handle socket.io errors.
+        socket.on('error', this.onSocketError);
+        // Set user socket as state variable.
+        this.setState({socket});
+    };
+
+    /**
+     * Handle user data event (after connection).
+     * @param user
+     * @param contacts
+     * @param messages
+     * @param users
+     */
+    onData = (user, contacts, messages, users) => {
+        let contact = contacts[0] || {};
+        this.setState({messages, contacts, user, contact}, () => {
+            this.updateUsersState(users);
+        });
+    };
+
+    /**
+     * Handle new user event.
+     * @param user
+     */
+    onNewUser = user => {
+        // Add user to contacts list.
+        let contacts = this.state.contacts.concat(user);
+        this.setState({contacts});
+    };
+
+    /**
+     * Handle update user event.
+     * @param user
+     */
+    onUpdateUser = user => {
+        // Add updated user is the current user then update local storage data.
+        if (this.state.user.id === user.id) {
+            this.setState({user});
+            Auth.setUser(user);
+            return;
+        }
+        // Update contact data.
+        let contacts = this.state.contacts;
+        contacts.forEach((element, index) => {
+            if(element.id === user.id) {
+                contacts[index] = user;
+                contacts[index].status = element.status;
+            }
+        });
+        this.setState({contacts});
+        if (this.state.contact.id === user.id) this.setState({contact: user});
+    };
+
+    /**
+     * Handle incoming message event.
+     * @param message
+     */
+    onNewMessage = message => {
+        // If user is already in chat then mark the message as seen.
+        if(message.sender === this.state.contact.id){
+            this.setState({typing: false});
+            this.state.socket.emit('seen', this.state.contact.id);
+            message.seen = true;
+        }
+        // Add message to messages list.
+        let messages = this.state.messages.concat(message);
+        this.setState({messages});
+    };
+
+    /**
+     * Handle typing or composing event.
+     * @param sender
+     */
+    onTypingMessage = sender => {
+        // If the typer not the current chat user then ignore it.
+        if(this.state.contact.id !== sender) return;
+        // Set typer.
+        this.setState({typing: sender});
+        // Create timeout function to remove typing status after 3 seconds.
+        clearTimeout(this.state.timeout);
+        const timeout = setTimeout(this.typingTimeout, 3000);
+        this.setState({timeout});
+    };
+
+    onSocketError = err => {
+        // If authentication error then logout.
+        if(err === 'auth_error'){
+            Auth.logout();
+            this.props.history.push('/login');
+        }
+    };
+
+    /**
+     * Clear typing status.
+     */
+    typingTimeout = () => this.setState({typing: false});
+
+    /**
+     * Send message.
+     * @param message
+     */
+    sendMessage = message => {
+        if(!this.state.contact.id) return;
+        message.receiver = this.state.contact.id;
+        let messages = this.state.messages.concat(message);
+        this.setState({messages});
+        this.state.socket.emit('message', message);
+    };
+
+    /**
+     * Send typing(composing) message.
+     */
+    sendType = () => this.state.socket.emit('typing', this.state.contact.id);
+
+    /**
+     * update users statuses.
+     * @param users
+     */
+    updateUsersState = users => {
+        let contacts = this.state.contacts;
+        contacts.forEach((element, index) => {
+            if(users[element.id]) contacts[index].status = users[element.id];
+        });
+        this.setState({contacts});
+        let contact = this.state.contact;
+        if(users[contact.id]) contact.status = users[contact.id];
+        this.setState({contact});
+    };
 
 }
 
